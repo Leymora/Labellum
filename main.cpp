7f21806ccffd4c7ba3ed2e46a57feb62d1000dd1
Vilhelm <13785032+Leymora@users.noreply.github.com>
2023-11-19 11:26:31 +0100

    Yeh

---
+++ main.cpp
@@ -0,0 +1,124 @@
+// Abisko Engine - Vilhelm Hansson / Vespera Chromatic -- Started 18-11-2023
+
+// cont https://docs.vulkan.org/tutorial/latest/03_Drawing_a_triangle/00_Setup/02_Validation_layers.html
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <vector>
+
+const uint16_t WINDOW_WIDTH = 800;
+const uint16_t WINDOW_HEIGHT = 600;
+const char **glfwExtensions;
+
+/// @brief Creates a triangle, lmao
+class HelloTriangleApplication
+{
+public:
+    void run()
+    {
+        initVulkan();
+        checkExtensions();
+        initWindow();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    GLFWwindow *window = nullptr;
+    VkInstance instance = NULL;
+    uint32_t glfwExtensionCount = 0;
+
+    bool checkExtensions()
+    {
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+        std::cout << "Available Extensions:\n";
+        for (const auto &extension : extensions)
+        {
+            std::cout << "\t" << extension.extensionName << "\n";
+        }
+
+        return false;
+    }
+
+    void createInstance()
+    {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_API_VERSION(0, 1, 0, 0);
+        appInfo.pEngineName = "Abisko Engine";
+        appInfo.engineVersion = VK_MAKE_API_VERSION(0, 1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_3;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+        if (result != VK_SUCCESS)
+        {
+            throw std::runtime_error("Failed to create instance.");
+        }
+    }
+
+    void initVulkan()
+    {
+        createInstance();
+    }
+
+    void initWindow()
+    {
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Abisko Engine", nullptr, nullptr);
+    }
+    void mainLoop()
+    {
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup()
+    {
+        vkDestroyInstance(instance, nullptr);
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+    }
+};
+
+int main()
+{
+    HelloTriangleApplication app;
+
+    try
+    {
+        app.run();
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
